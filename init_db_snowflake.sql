4f8b896859428ee9b1ed87b3c25fe3e4a55257af
positronn <88909413+quasitensor@users.noreply.github.com>
2021-12-01 13:58:25 -0600

    initial upload.

---
+++ init_db_snowflake.sql
@@ -0,0 +1,34 @@
+-------------------------------------------
+-- dbt credentials
+-------------------------------------------
+USE ROLE securityadmin;
+-- dbt roles
+CREATE OR REPLACE ROLE dbt_dev_role;
+CREATE OR REPLACE ROLE dbt_prod_role;
+------------------------------------------- Please replace with your dbt user password
+CREATE OR REPLACE USER dbt_user PASSWORD = "tecmilenio.dbt";
+
+GRANT ROLE dbt_dev_role,dbt_prod_role TO USER dbt_user;
+GRANT ROLE dbt_dev_role,dbt_prod_role TO ROLE sysadmin;
+
+-------------------------------------------
+-- dbt objects
+-------------------------------------------
+USE ROLE sysadmin;
+
+CREATE OR REPLACE WAREHOUSE dbt_dev_wh  WITH WAREHOUSE_SIZE = 'XSMALL' AUTO_SUSPEND = 60 AUTO_RESUME = TRUE MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1 INITIALLY_SUSPENDED = TRUE;
+CREATE OR REPLACE WAREHOUSE dbt_dev_heavy_wh  WITH WAREHOUSE_SIZE = 'LARGE' AUTO_SUSPEND = 60 AUTO_RESUME = TRUE MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1 INITIALLY_SUSPENDED = TRUE;
+CREATE OR REPLACE WAREHOUSE dbt_prod_wh WITH WAREHOUSE_SIZE = 'XSMALL' AUTO_SUSPEND = 60 AUTO_RESUME = TRUE MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1 INITIALLY_SUSPENDED = TRUE;
+CREATE OR REPLACE WAREHOUSE dbt_prod_heavy_wh  WITH WAREHOUSE_SIZE = 'LARGE' AUTO_SUSPEND = 60 AUTO_RESUME = TRUE MIN_CLUSTER_COUNT = 1 MAX_CLUSTER_COUNT = 1 INITIALLY_SUSPENDED = TRUE;
+
+GRANT ALL ON WAREHOUSE dbt_dev_wh  TO ROLE dbt_dev_role;
+GRANT ALL ON WAREHOUSE dbt_dev_heavy_wh  TO ROLE dbt_dev_role;
+GRANT ALL ON WAREHOUSE dbt_prod_wh TO ROLE dbt_prod_role;
+GRANT ALL ON WAREHOUSE dbt_prod_heavy_wh  TO ROLE dbt_prod_role;
+
+CREATE OR REPLACE DATABASE dbt_hol_dev; 
+CREATE OR REPLACE DATABASE dbt_hol_prod; 
+GRANT ALL ON DATABASE dbt_hol_dev  TO ROLE dbt_dev_role;
+GRANT ALL ON DATABASE dbt_hol_prod TO ROLE dbt_prod_role;
+GRANT ALL ON ALL SCHEMAS IN DATABASE dbt_hol_dev   TO ROLE dbt_dev_role;
+GRANT ALL ON ALL SCHEMAS IN DATABASE dbt_hol_prod  TO ROLE dbt_prod_role;
